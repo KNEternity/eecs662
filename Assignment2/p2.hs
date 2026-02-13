91c6250d9aa0f2808f408020096e16c656ff3227
Kevinh Nguyen <kevinh@ku.edu>
2026-02-13 10:37:40 -0600

    Assignment 2 - Part 1 Done

---
+++ Assignment2/p2.hs
@@ -0,0 +1,111 @@
+{-# LANGUAGE GADTs, FlexibleContexts #-}
+
+
+-- AST Definition
+data KUTypeLang where
+    TNum :: KUTypeLang
+    TBool :: KUTypeLang 
+    deriving (Show,Eq)
+
+data KULang where
+    Num :: Int -> KULang 
+    Boolean :: Bool -> KULang
+    Plus :: KULang -> KULang -> KULang 
+    Minus :: KULang -> KULang -> KULang  
+    Mult :: KULang -> KULang -> KULang  
+    Div :: KULang -> KULang -> KULang   
+    Exp :: KULang -> KULang -> KULang 
+    And :: KULang -> KULang -> KULang   
+    Or :: KULang -> KULang -> KULang  
+    Leq :: KULang -> KULang -> KULang  
+    IsZero :: KULang -> KULang  
+    If :: KULang -> KULang -> KULang -> KULang  
+    Between :: KULang -> KULang -> KULang -> KULang  
+    deriving (Show,Eq)
+
+-------------------------------
+------ Project Exercises ------
+-------------------------------
+-- Part 1: Type Inference
+
+-- Exercise 1
+evalMonad :: KULang -> Maybe KULang
+evalMonad (Boolean b) = Just (Boolean b)
+evalMonad (Num x) = if x < 0 then Nothing else Just (Num x) --force the type to be num
+evalMonad (Plus l r) = do {
+    (Num l') <- evalMonad l;
+    (Num r') <- evalMonad r;
+    Just (Num (l'+r'))
+}
+evalMonad (Minus l r ) = do {
+    (Num l') <- evalMonad l;
+    (Num r') <- evalMonad r;
+    (Num d) <- Just (Num(l'-r')); --I think I'm supposed to force d to be num type
+    if d < 0 then Nothing else Just (Num d) --then do it again?
+}
+evalMonad (Mult l r) = do {
+    (Num l') <- evalMonad l;
+    (Num r') <- evalMonad r;
+    Just (Num (l'*r'))
+}
+evalMonad (Div l r ) = do {
+    (Num l') <- evalMonad l;
+    (Num r') <- evalMonad r;
+    if r' == 0 then Nothing else Just (Num (l' `div` r'))
+}
+evalMonad (Exp l r) = do{
+    (Num l') <- evalMonad l;
+    (Num r') <- evalMonad r;
+    Just (Num (l'^r'))   
+}
+evalMonad (And l r) = do{
+    (Boolean l') <- evalMonad l;
+    (Boolean r') <- evalMonad r;
+    Just (Boolean (l'&&r'))
+}
+evalMonad (Or l r) = do {
+    (Boolean l') <- evalMonad l;
+    (Boolean r') <- evalMonad r;
+    Just (Boolean (l' || r'))
+}
+evalMonad (Leq l r) = do {
+    (Num l') <- evalMonad l;
+    (Num r') <- evalMonad r;
+    Just (Boolean (l' <= r'))
+}
+evalMonad (IsZero x) = do {
+    (Num x') <- evalMonad x;
+    Just (Boolean (x' == 0))
+}
+evalMonad (If c t e) = do {
+    (Boolean c') <- evalMonad c;
+    t' <- evalMonad t;
+    e' <- evalMonad e;
+    if c' then Just t' else Just e'
+}
+evalMonad(Between x y z) = do {
+    (Num x') <- evalMonad x;
+    (Num y') <- evalMonad y;
+    (Num z') <- evalMonad z;
+    evalMonad (And (Boolean(x' < y'))(Boolean(y' < z'))) --oh my god im awesome
+    --Just (Boolean ((x' < y') && (y' < z')))
+}
+evalMonad _ = Nothing
+
+-- Exercise 2
+typeofMonad :: KULang -> Maybe KUTypeLang
+typeofMonad _ = Nothing
+
+-- Exercise 3
+interpTypeEval :: KULang -> Maybe KULang
+interpTypeEval _ = Nothing
+
+-- Part 2: Optimizer
+
+-- Exercise 1
+optimize :: KULang -> KULang
+optimize a = a
+
+-- Exercise 2
+interpOptEval :: KULang -> Maybe KULang
+interpOptEval _ = Nothing
