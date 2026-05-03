cff69f4b2b2e00e237f9073c624c9f3c50f7c98a
unknown <applekevinh@gmail.com>
2026-05-03 15:42:02 -0500

    finish assignment

--- Assignment5/p5.hs
+++ Assignment5/p5.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE GADTs, PostfixOperators #-}
 
 import Control.Monad
+import Data.Text.Array (run)
 
 data KUTypeLang where
   TNum :: KUTypeLang
@@ -47,6 +48,7 @@ data KULangExt where
   OrX :: KULangExt -> KULangExt -> KULangExt
   LeqX :: KULangExt -> KULangExt -> KULangExt
   IsZeroX :: KULangExt -> KULangExt
+  FixX :: KULangExt -> KULangExt
   deriving (Show,Eq)
 
 data KULangVal where
@@ -72,6 +74,26 @@ local f r = Reader $ \e -> runR r (f e)
 
 useClosure :: String -> KULangVal -> EnvVal -> EnvVal -> EnvVal
 useClosure i v e _ = (i,v):e
+
+-- Substitution helper
+subst :: String -> KULang -> KULang -> KULang
+subst i v (Id x)        = if x == i then v else Id x
+subst i v (Plus l r)    = Plus (subst i v l) (subst i v r)
+subst i v (Minus l r)   = Minus (subst i v l) (subst i v r)
+subst i v (Mult l r)    = Mult (subst i v l) (subst i v r)
+subst i v (Div l r)     = Div (subst i v l) (subst i v r)
+subst i v (Exp l r)     = Exp (subst i v l) (subst i v r)
+subst i v (Between x y z) = Between (subst i v x) (subst i v y) (subst i v z)
+subst i v (Lambda x d b)  = if x == i then Lambda x d b
+                             else Lambda x d (subst i v b)
+subst i v (App f a)     = App (subst i v f) (subst i v a)
+subst i v (If c t e')   = If (subst i v c) (subst i v t) (subst i v e')
+subst i v (And x y)     = And (subst i v x) (subst i v y)
+subst i v (Or x y)      = Or (subst i v x) (subst i v y)
+subst i v (Leq x y)     = Leq (subst i v x) (subst i v y)
+subst i v (IsZero x)    = IsZero (subst i v x)
+subst i v (Fix f)       = Fix (subst i v f)
+subst _ _ e             = e  -- Num, Boolean base cases
 
 instance Monad (Reader e) where
     g >>= f = Reader $ \e -> 
@@ -102,7 +124,7 @@ instance MonadFail (Reader e) where
 
 -- Part 1 - Type Inference
 typeof :: KULang -> Reader Cont KUTypeLang
-typeof (Num n) = if n < 0 then error "fail" else return (TNum)
+typeof (Num n) = if n < 0 then error "Num typefail" else return (TNum)
 typeof (Boolean b) = return (TBool)
 typeof (Id i) = do {cont <- ask;
                     case (lookup i cont) of
@@ -147,14 +169,14 @@ typeof (Lambda i d b) = do {
 
 typeof (App f a) = do {a' <- typeof a;
                          d :->: r <- typeof f;
-                         if a'==d then return r else error "fail"}
+                         if a'==d then return r else error "App type fail"}
                      
 
 typeof (If c t e') = do {
     TBool <- typeof c;
     typeT <- typeof t;
     typee <- typeof e';
-    if typeT == typee then return typeT else error "fail"}
+    if typeT == typee then return typeT else error "If type fail"}
 
 typeof (And x y) = do {
     TBool <- typeof x;
@@ -176,14 +198,14 @@ typeof (Leq x y) = do {
 
 typeof (IsZero x) = do {
     TNum <- typeof x;
-    return TNum
+    return TBool
 }
 typeof (Fix f) = do {(d :->: r) <- typeof f;
-                        if d == r then return d else error "fail"}
+                        if d == r then return d else error "Fix type fail"}
 typeof _ = fail "Not implemented yet"
 -- Part 2 - Evaluation
 eval :: KULang -> Reader EnvVal KULangVal
-eval (Num n) = if n<0 then error "fail" else return (NumV n)
+eval (Num n) = if n<0 then error "Num eval fail" else return (NumV n)
 eval (Boolean b) = return (BooleanV b)
 eval (Id i) = do {env <- ask;
                     case (lookup i env) of
@@ -198,7 +220,7 @@ eval (Minus l r ) = do {
     (NumV l') <- eval l;
     (NumV r') <- eval r;
     let d = l' - r' in
-    if d < 0 then error "fail" else return (NumV d)
+    if d < 0 then error "Minus eval fail" else return (NumV d)
 }
 eval (Mult l r) = do {
     (NumV l') <- eval l;
@@ -208,7 +230,7 @@ eval (Mult l r) = do {
 eval (Div l r ) = do {
     (NumV l') <- eval l;
     (NumV r') <- eval r;
-    if r' == 0 then error "fail" else return (NumV (l' `div` r'))
+    if r' == 0 then error "Div eval fail" else return (NumV (l' `div` r'))
 
 }
 
@@ -227,13 +249,14 @@ eval(Between x y z) = do {
 
 eval (Lambda i d b) = do {env <- ask; --d stands for the domain type
                     return (ClosureV i b env)} --this is because we are static scoping
+
 eval (App f a) = do {
                      (ClosureV i b e) <- eval f;
                      v <- eval a;
                      local (useClosure i v e) (eval b)}
 eval (If c t e') = do {
-    (NumV c') <- eval c;
-    if c' == 0 then (eval t) else (eval e') }
+    (BooleanV c') <- eval c;
+    if c' then eval t else eval e' }
 
 eval (And x y) = do {
     (BooleanV x') <- eval x;
@@ -255,9 +278,8 @@ eval (IsZero x) = do {
     return (BooleanV (x' == 0))}   
 
 eval (Fix f) = do
-    (ClosureV i b env) <- eval f
-    let rec_closure = ClosureV i b ((i, rec_closure) : env)
-    local (useClosure i rec_closure env) (eval b)
+    (ClosureV i b _) <- eval f
+    eval (subst i (Fix (Lambda i TNum b)) b)
 
 eval _ = fail "Not implemented yet"
 
@@ -276,39 +298,65 @@ eval _ = fail "Not implemented yet"
 --     if d == r then return d else error "fail"}
 
 -- Part 4 - Interpretation
+--Elaborator that converts KULangExt to KULang
+elabTerm :: KULangExt -> KULang
+elabTerm (NumX n) = Num n
+elabTerm (BooleanX b) = Boolean b
+elabTerm (IdX id) = Id id
+elabTerm (PlusX l r) = Plus (elabTerm l) (elabTerm r)
+elabTerm (MinusX l r) = Minus (elabTerm l) (elabTerm r)
+elabTerm (MultX l r) = Mult (elabTerm l) (elabTerm r)
+elabTerm (DivX l r) = Div (elabTerm l) (elabTerm r)
+elabTerm (ExpX l r) = Exp (elabTerm l) (elabTerm r)
+elabTerm (BetweenX x y z) = Between (elabTerm x) (elabTerm y) (elabTerm z)
+elabTerm (LambdaX i d b) = Lambda i d (elabTerm b)
+elabTerm (AppX f a) = App (elabTerm f) (elabTerm a)
+elabTerm (BindX i d v b) = App (Lambda i d (elabTerm b)) (elabTerm v)
+elabTerm (IfX c t e') = If (elabTerm c) (elabTerm t) (elabTerm e')
+elabTerm (AndX x y) = And (elabTerm x) (elabTerm y)
+elabTerm (OrX x y) = Or (elabTerm x) (elabTerm y)
+elabTerm (LeqX x y) = Leq (elabTerm x) (elabTerm y)
+elabTerm (IsZeroX x) = IsZero (elabTerm x)
+elabTerm (FixX f) = Fix (elabTerm f)
+elabTerm _ = (Num (-1))
+
 interpret :: KULangExt -> Maybe KULangVal
+interpret t = do {
+    runR (typeof (elabTerm t)) []; --run this to check type
+    runR (eval (elabTerm t)) [] --run this to evaluate
+}
 interpret _ = Nothing
 
 -- Test Cases
 
--- testFib = interpret ( 
---                 BindX "fib" (TNum :->: TNum)
---                     (FixX (LambdaX "g" (TNum :->: TNum)
---                         (LambdaX "x" TNum 
---                             (IfX (LeqX (IdX "x") (NumX 1))
---                                 (IdX "x")
---                                 (PlusX 
---                                     (AppX (IdX "g") (MinusX (IdX "x") (NumX 1)))
---                                     (AppX (IdX "g") (MinusX (IdX "x") (NumX 2)))
---                                 )
---                             )
---                         )
---                     ))
---                     (AppX (IdX "fib") (NumX 2))) == 
---                 Just (NumV 1)
+testFib = interpret ( 
+                BindX "fib" (TNum :->: TNum)
+                    (FixX (LambdaX "g" (TNum :->: TNum)
+                        (LambdaX "x" TNum 
+                            (IfX (LeqX (IdX "x") (NumX 1))
+                                (IdX "x")
+                                (PlusX 
+                                    (AppX (IdX "g") (MinusX (IdX "x") (NumX 1)))
+                                    (AppX (IdX "g") (MinusX (IdX "x") (NumX 2)))
+                                )
+                            )
+                        )
+                    ))
+                    (AppX (IdX "fib") (NumX 2))) == 
+                Just (NumV 1)
 
--- testIsEven = interpret ( 
---                 BindX "isEven" (TNum :->: TBool)
---                     (FixX (LambdaX "g" (TNum :->: TBool)
---                         (LambdaX "x" TNum
---                             (IfX (IsZeroX (IdX "x"))
---                                 (BooleanX True)
---                                 (IfX (LeqX (IdX "x") (NumX 1))
---                                     (BooleanX False)
---                                     (AppX (IdX "g") (MinusX (IdX "x") (NumX 2)))
---                                 )
---                             )
---                         )
---                     ))
---                     (AppX (IdX "isEven") (NumX 67))) == 
---                 Just (BooleanV False)
+testIsEven = interpret ( 
+                BindX "isEven" (TNum :->: TBool)
+                    (FixX (LambdaX "g" (TNum :->: TBool)
+                        (LambdaX "x" TNum
+                            (IfX (IsZeroX (IdX "x"))
+                                (BooleanX True)
+                                (IfX (LeqX (IdX "x") (NumX 1))
+                                    (BooleanX False)
+                                    (AppX (IdX "g") (MinusX (IdX "x") (NumX 2)))
+                                )
+                            )
+                        )
+                    ))
+                    (AppX (IdX "isEven") (NumX 67))) == 
+                    Just (BooleanV False)
